d1e6ab2cb496c2a23fc7cbf722b4c137d32367d2
iamekber <80426584+iamekber@users.noreply.github.com>
2021-03-14 22:33:43 +0300

    Add project files.

---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace DictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> liste = new Dictionary<string, int>();
+            liste.Add("Ali", 0);
+            liste.Add("Gülşah", 1);
+            liste.Add("Mucize", 2);
+            
+
+            foreach (KeyValuePair<string, int> item in liste)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value.ToString());
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
